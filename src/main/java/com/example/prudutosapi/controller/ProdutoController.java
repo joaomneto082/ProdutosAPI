b6ef488998e8590eb610e52ebccdf52d6e69bcde
Joaoneto082 <joaoneto082@gmail.com>
2024-11-04 18:29:42 -0300

    commit inicial

---
+++ src/main/java/com/example/prudutosapi/controller/ProdutoController.java
@@ -0,0 +1,33 @@
+package com.example.prudutosapi.controller;
+
+import com.example.prudutosapi.model.Produto;
+import com.example.prudutosapi.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto){
+        System.out.println("Produto recebido: " + produto);
+
+        var id = UUID.randomUUID().toString();  //seta um id aleatorio
+        produto.setId(id);
+
+        produtoRepository.save(produto);
+        return produto;
+
+    }
+}
