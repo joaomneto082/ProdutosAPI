2d82cc30ce3e0c6d905777a733a6a98bd4e64ad2
joaomneto082 <joao.mneto@dataprev.gov.br>
2024-11-06 09:40:50 -0300

    funcao buscar

--- src/main/java/com/example/prudutosapi/controller/ProdutoController.java
+++ src/main/java/com/example/prudutosapi/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import com.example.prudutosapi.model.Produto;
 import com.example.prudutosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -43,5 +44,10 @@ public class ProdutoController {
                           @RequestBody Produto produto){
         produto.setId(id);
         produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome){
+        return produtoRepository.findByNome(nome);
     }
 }
